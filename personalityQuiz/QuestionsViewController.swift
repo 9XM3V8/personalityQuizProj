3a4a7c47afd5c6584a0fea7089c0693d4dd0fb2c
Dylan <dylanlouder@gmail.com>
2024-10-08 21:17:55 -0600

    updated

--- personalityQuiz/QuestionsViewController.swift
+++ personalityQuiz/QuestionsViewController.swift
@@ -6,18 +6,101 @@
 //
 
 import UIKit
-
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+enum ResponseType {
+    case single, multiple, ranged
+}
+struct Answer {
+    var text: String
+    var type: CharacterType
+}
+enum CharacterType: String {
+    case cartman = "showCartman", squidward = "showSquiddy", joker = "showJoker", freddy = "showFreddy"
+    
+    var definition: String {
+        switch self {
+        case .cartman:
+            return "Eric Cartman is a central character in South Park, known for his manipulative and selfish behavior. He often displays a lack of empathy."
+        case .squidward:
+            return "Squidward Tentacles is a character from SpongeBob SquarePants, known for his grumpy demeanor and disdain for SpongeBob."
+        case .joker:
+            return "The Joker is a iconic villain from Batman, known for his maniacal laughter, chaotic antics, and twisted sense of humor."
+        case .freddy:
+            return "Freddy Fazbear is the central character from the Five Nights at Freddy's (FNAF) video game series. He is an animatronic bear."
+        }
+    }
+}
 class QuestionsViewController: UIViewController {
     @IBOutlet weak var octDec: UIButton!
     @IBOutlet weak var julSep: UIButton!
     @IBOutlet weak var aprJun: UIButton!
     @IBOutlet weak var janMar: UIButton!
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var rangedStackView: UIStackView!
+    var questionIndex = 0
+    var questions: [Question] = [
+        Question(
+            text: "What section does your birth month fall under?",
+            type: .single,
+            answers: [
+                Answer(text: "January - March", type: .cartman),
+                Answer(text: "April - June", type: .squidward),
+                Answer(text: "July - September", type: .joker),
+                Answer(text: "October - December", type: .freddy)
+            ]
+        ),
+    
+        Question(
+            text: "What activities do you enjoy?",
+            type: .multiple,
+            answers: [
+                Answer(text: "Malicious Crime", type: .joker),
+                Answer(text: "Biking", type: .squidward),
+                Answer(text: "Making Pizza", type: .freddy),
+                Answer(text: "Gaming", type: .cartman)
+            ]
+        ),
+        
+        Question(
+            text: "Livy Dunne or Chung Li?",
+            type: .ranged,
+            answers: [
+                Answer(text: "Livy Dunne", type: .joker),
+                Answer(text: "More So Livy Dunne", type: .squidward),
+                Answer(text: "More So Chung Li", type: .cartman),
+                Answer(text: "Chung Li", type: .freddy)
+                ]
+            )
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Question #\(questionIndex + 1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         guard let sender = sender as? String, let destinationVC = segue.destination as? LandingViewController else {
